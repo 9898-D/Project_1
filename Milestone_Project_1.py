adbd8150feb7f70a510db4b04048ce159a83fdc6
ALPINA  B7 <choubisadh2000.dc@gmail.com>
2021-02-17 20:36:15 +0530

    MAIN FILE

---
+++ Milestone_Project_1.py
@@ -0,0 +1,22 @@
+# MILESSTONE - PROJECT-1 
+
+#DISPLAYING INFO LIKE STRUCTURE OF TIC TIK TAC GAME
+#LIKE THIS
+# [1,2,3]
+# [4,5,6]
+# [7,8,9]
+
+def display(row1,row2,row3):
+    print(row1)
+    print(row2)
+    print(row3)
+row1=[' ',' ',' ']
+row2=[' ',' ',' ']
+row3=[' ',' ',' ']
+
+display(row1,row2,row3)
+
+row2[1]='X'
+display(row1,row2,row3)
+
+## ACCEPT USER INPUT 
